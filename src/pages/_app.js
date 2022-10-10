55cfd28bea2a3e54000e4a9e7a66bbf62ff03351
AOMuiz <abdulwasiumuiz@gmail.com>
2022-10-10 17:24:23 +0100

    fix:not reloading and changing dir attribute on language change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
+import { useRouter } from "next/router";
+
 import "../../styles/globals.css";
 import { appWithTranslation } from "next-i18next";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const { locale } = useRouter();
+  const dir = locale === "ar" ? "rtl" : "ltr";
+
+  useEffect(() => {
+    document.documentElement.dir = dir;
+  }, [dir]);
+
   return <Component {...pageProps} />;
 }
 
