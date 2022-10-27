da0cb15c62b73c39f0e828b06da9e79b77635b7d
Lukman10a <abdulrauflukman9@gmail.com>
2022-10-27 15:47:48 +0100

    add some changes

---
+++ src/components/Cards/card.js
@@ -0,0 +1,9 @@
+import useTranslation from "next-translate/useTranslation";
+import React from "react";
+
+const Card = () => {
+  const { t } = useTranslation("index");
+  return <div>card</div>;
+};
+
+export default Card;
