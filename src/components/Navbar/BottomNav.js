a6c70b9ed06fda3f972ec6c4e7ba1f46e54a7549
AOMuiz <abdulwasiumuiz@gmail.com>
2022-10-28 21:20:39 +0100

    refactor: change all tw attribut to classname

--- src/components/Navbar/BottomNav.js
+++ src/components/Navbar/BottomNav.js
@@ -7,7 +7,7 @@ const BottomNav = () => {
   const { t } = useTranslation("index");
 
   return (
-    <div tw="flex px-16 py-4 justify-between items-center">
+    <div tw="flex px-16 py-4 justify-between items-center shadow-sm">
       <div tw="flex items-center gap-6">
         <Logo width={80} height={60} tw="cursor-pointer" />
         <p tw="flex justify-center items-center border-2 border-primary-P300 rounded-full px-2 h-auto py-1 cursor-pointer">
@@ -15,7 +15,7 @@ const BottomNav = () => {
             <Icon id={"menu"} />
           </span>
           {/* fix padding here for english */}
-          <span tw="text-black pl-2">{t("navbar.menu")}</span>
+          <span className="text-black pie-2">{t("navbar.menu")}</span>
         </p>
       </div>
       <div
--- src/components/Navbar/TopNav.js
+++ src/components/Navbar/TopNav.js
@@ -7,18 +7,18 @@ const TopNav = () => {
   const { t } = useTranslation("index");
   return (
     <>
-      <div tw="bg-white flex px-16 font-bold justify-between py-3 border-b-2 border-gray-G20">
-        <div tw="flex justify-between gap-4 text-gray-G30 items-baseline">
-          <p tw="cursor-pointer">{t("navbar.Join us")}</p>
-          <p tw="cursor-pointer">{t("navbar.corporate training")}</p>
-          <p tw="cursor-pointer">{t("navbar.blog")}</p>
+      <div className="bg-white flex px-16 font-bold justify-between py-3 border-b-2 border-gray-G20">
+        <div className="flex justify-between gap-4 text-gray-G30 items-baseline">
+          <p className="cursor-pointer">{t("navbar.Join us")}</p>
+          <p className="cursor-pointer">{t("navbar.corporate training")}</p>
+          <p className="cursor-pointer">{t("navbar.blog")}</p>
           <LocaleSwitcher />
         </div>
-        <div tw="flex gap-4 text-[#D5D5D5] items-center">
-          <Icon id={"youtube"} tw="cursor-pointer" />
-          <Icon id={"facebook"} tw="cursor-pointer" />
-          <Icon id={"instagram"} tw="cursor-pointer" />
-          <Icon id={"twitter"} tw="cursor-pointer" />
+        <div className="flex gap-4 text-[#D5D5D5] items-center">
+          <Icon id={"youtube"} className="cursor-pointer" />
+          <Icon id={"facebook"} className="cursor-pointer" />
+          <Icon id={"instagram"} className="cursor-pointer" />
+          <Icon id={"twitter"} className="cursor-pointer" />
         </div>
       </div>
     </>
