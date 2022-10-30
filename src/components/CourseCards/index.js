709852f0b849558a3be5226f3ff9c4f054a6493c
Lukman10a <abdulrauflukman9@gmail.com>
2022-10-30 17:09:03 +0100

    create coursecard component

---
+++ src/components/CourseCards/index.js
@@ -0,0 +1,9 @@
+import useTranslation from "next-translate/useTranslation";
+import React from "react";
+
+const CourseCards = () => {
+  const { t } = useTranslation("index");
+  return <div>Coursecard</div>;
+};
+
+export default CourseCards;
