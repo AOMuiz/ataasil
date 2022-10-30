8cff2f0be4304663043953bccbf6896b7db8435b
Lukman10a <abdulrauflukman9@gmail.com>
2022-10-30 18:17:47 +0100

    add styles to the coursecards component

--- src/components/CourseCards/index.js
+++ src/components/CourseCards/index.js
@@ -3,7 +3,20 @@ import React from "react";
 
 const CourseCards = () => {
   const { t } = useTranslation("index");
-  return <div>Coursecard</div>;
+
+  return (
+    <div>
+      <div tw="flex justify-between w-[284.09px] h-[404px] rounded-[30px] bg-[#FFFFFF]">
+        <div tw="cursor-pointer">
+          <p>{t("coursecards.topic")}</p>
+        </div>
+        <div>
+          <p tw="cursor-pointer">{t("coursecards.date")}</p>
+        </div>
+      </div>
+      <p tw="cursor-pointer">{t("coursecards.title")}</p>
+    </div>
+  );
 };
 
 export default CourseCards;
