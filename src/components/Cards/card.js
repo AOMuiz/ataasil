82537f90056b422bc5b88a4bd3fb5e39c2b047da
Lukman10a <abdulrauflukman9@gmail.com>
2022-10-30 17:08:42 +0100

    rename card component

--- src/components/Cards/card.js
+++
@@ -1,9 +0,0 @@
-import useTranslation from "next-translate/useTranslation";
-import React from "react";
-
-const Card = () => {
-  const { t } = useTranslation("index");
-  return <div>card</div>;
-};
-
-export default Card;
