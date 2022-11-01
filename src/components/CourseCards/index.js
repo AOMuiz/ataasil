8217453fc72926242f01ccfec74c96b49bedd4b4
AOMuiz <abdulwasiumuiz@gmail.com>
2022-11-01 07:51:43 +0100

    chore: move coursecard title into index and delete the course section component

--- src/components/CourseCards/index.js
+++ src/components/CourseCards/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import CourseCard from "./coursecard";
-import SectionTitle from "./sectiontitle";
 import SectionCategory from "./sectioncategory";
 import Icon from "../Icon";
-import CourseNav from "./coursenav";
+import useTranslation from "next-translate/useTranslation";
 
 const CourseCards = () => {
+  const { t } = useTranslation("index");
   return (
     <div tw="flex flex-col bg-neutral-N30 w-full py-10">
       <div tw="px-60">
-        <SectionTitle />
+        <div tw="text-3xl	font-bold">{t("coursecards.main topic")}</div>
       </div>
       <div tw="flex gap-3 justify-center">
         <SectionCategory />
@@ -28,13 +28,19 @@ const CourseCards = () => {
           <CourseCard />
         </div>
         <div>
-          <div tw="flex gap-2 flex-row-reverse px-60 py-8">
-            <div tw=" bg-[#35CCBC80] p-3 rounded-md h-12 w-12 cursor-pointer">
-              <Icon id={"left"} color={"white"} size={25} />
-            </div>
-            <div tw="bg-[#35CCBC] p-3 rounded-md h-12 w-12">
-              <Icon id={"right"} color={"white"} size={25} />
-            </div>
+          <div tw="flex gap-2 flex-row-reverse">
+            <Icon
+              id={"left"}
+              color={"white"}
+              size={25}
+              tw=" bg-[#35CCBC80] p-3 rounded-md h-12 w-12 cursor-pointer"
+            />
+            <Icon
+              id={"right"}
+              color={"white"}
+              size={25}
+              tw="bg-[#35CCBC] p-3 rounded-md h-12 w-12"
+            />
           </div>
         </div>
       </section>
--- src/components/CourseCards/sectiontitle.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import useTranslation from "next-translate/useTranslation";
-
-const SectionTitle = () => {
-  const { t } = useTranslation("index");
-  return <div tw="text-3xl	font-bold">{t("coursecards.main topic")}</div>;
-};
-
-export default SectionTitle;
