12085131dda182d0aa232fb9c7011f18ca13bfa9
AOMuiz <abdulwasiumuiz@gmail.com>
2022-11-01 07:58:12 +0100

    chore: add translation for interest section description and change all tw attribute for interest card to classname

--- src/components/InterestCard/index.js
+++ src/components/InterestCard/index.js
@@ -7,17 +7,17 @@ import interest_image from "/public/assets/images/interest_img.png";
 const InterestCard = () => {
   const { t } = useTranslation("index");
   return (
-    <div tw="flex justify-center gap-8 p-16">
-      <div tw=" w-[550px] p-8">
-        <p tw="font-bold text-3xl py-6">{t("interestcard.header")}</p>
-        <p tw="py-14">{t("interestcard.contents")}</p>
-        <div tw="flex gap-4 items-center justify-center cursor-pointer rounded-3xl w-28 bg-primary-P600 p-4 text-white ">
+    <section className="flex justify-center gap-8 p-16">
+      <div className="w-[550px] p-8">
+        <p className="font-bold text-3xl py-6">{t("interestcard.header")}</p>
+        <p className="py-14">{t("interestcard.contents")}</p>
+        <div className="flex gap-4 items-center justify-center cursor-pointer rounded-3xl w-28 bg-primary-P600 p-4 text-white ">
           <p>{t("interestcard.begin")}</p>
           <Icon id={"left"} />
         </div>
       </div>
       <Image src={interest_image} alt="" />
-    </div>
+    </section>
   );
 };
 
