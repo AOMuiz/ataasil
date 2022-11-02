bb67416c996f36644fc04122e92f114cb7177bff
AOMuiz <abdulwasiumuiz@gmail.com>
2022-11-02 09:01:06 +0100

    refactor: move interstcard translation object into home object and add max-width for the interestcard section

--- src/components/InterestCard/index.js
+++ src/components/InterestCard/index.js
@@ -8,12 +8,12 @@ import SectionHeader from "../SectionHeader";
 const InterestCard = () => {
   const { t } = useTranslation("index");
   return (
-    <div className="flex justify-between items-center gap-20 p-20">
-      <div className="w-[800px]">
-        <SectionHeader title={t("interestcard.header")} />
-        <p className="py-16">{t("interestcard.contents")}</p>
+    <div className="flex justify-between items-center gap-20 px-20 py-10">
+      <div className="w-fit max-w-[600px]">
+        <SectionHeader title={t("home.interestcard.header")} />
+        <p className="py-16">{t("home.interestcard.contents")}</p>
         <div className="flex gap-4 items-center justify-center cursor-pointer rounded-3xl w-28 bg-primary-P600 p-4 text-white ">
-          <p>{t("interestcard.begin")}</p>
+          <p>{t("home.interestcard.begin")}</p>
           <Icon id={"left"} />
         </div>
       </div>
