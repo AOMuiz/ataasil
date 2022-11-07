24d9ff43d90e39ea9b1d10389fdee43cb9e0d487
AOMuiz <abdulwasiumuiz@gmail.com>
2022-11-07 17:33:12 +0100

    refactor: increase padding for bot tgop and bottom navbar, fix paddin for Join button and increase icons font size

--- src/components/Navbar/TopNav.js
+++ src/components/Navbar/TopNav.js
@@ -7,14 +7,14 @@ const TopNav = () => {
   const { t } = useTranslation("index");
   return (
     <>
-      <div className="bg-white flex px-20 font-bold justify-between py-3 border-b-2 border-gray-G20">
+      <div className="bg-white flex px-20 py-4 font-bold justify-between border-b-2 border-gray-G20">
         <div className="flex justify-between gap-4 text-gray-G30 items-baseline">
           <p className="cursor-pointer">{t("navbar.Join us")}</p>
           <p className="cursor-pointer">{t("navbar.corporate training")}</p>
           <p className="cursor-pointer">{t("navbar.blog")}</p>
           <LocaleSwitcher />
         </div>
-        <div className="flex gap-4 text-[#D5D5D5] items-center">
+        <div className="flex gap-5 text-[#D5D5D5] items-center text-2xl">
           <Icon id={"youtube"} className="cursor-pointer" />
           <Icon id={"facebook"} className="cursor-pointer" />
           <Icon id={"instagram"} className="cursor-pointer" />
