edd65fc67d0a7e6c4266f89b61175789eedf0375
AOMuiz <abdulwasiumuiz@gmail.com>
2022-11-07 17:33:12 +0100

    refactor: increase padding for bot tgop and bottom navbar, fix paddin for Join button and increase icons font size

--- src/components/Navbar/BottomNav.js
+++ src/components/Navbar/BottomNav.js
@@ -7,28 +7,27 @@ const BottomNav = () => {
   const { t } = useTranslation("index");
 
   return (
-    <div className="flex px-20 py-4 justify-between items-center shadow-sm">
+    <div className="flex px-20 py-5 justify-between items-center shadow-sm">
       <div className="flex items-center gap-6">
         <Logo width={80} height={60} className="cursor-pointer" />
-        <p className="flex justify-center items-center border-2 border-primary-P300 rounded-full px-2 h-auto py-1 cursor-pointer">
-          <span className="p-2">
+        <p className="flex justify-center items-center border-2 border-primary-P300 rounded-full py-2 px-3 h-auto cursor-pointer gap-3">
+          <span>
             <Icon id={"menu"} />
           </span>
-          {/* fix padding here for english */}
-          <span className="text-black pl-2">{t("navbar.menu")}</span>
+          <span className="text-black">{t("navbar.menu")}</span>
         </p>
       </div>
-      <div className="flex items-center basis-1/3 bg-gray-G20 h-auto rounded-full overflow-hidden py-2 w-auto">
+      <div className="flex items-center basis-1/3 bg-gray-G20 h-auto rounded-full overflow-hidden w-auto">
         <Icon id={"search"} className="text-primary-P300 px-3" size={20} />
         <input
           type="text"
-          className="bg-gray-G20 outline-none w-full placeholder:text-black placeholder:font-light placeholder:py-1"
+          className="bg-gray-G20 outline-none w-full placeholder:text-black placeholder:font-light placeholder:py-1 py-3 h-full"
           placeholder={t("navbar.search placeholder")}
         />
       </div>
       <div className="flex items-center justify-evenly gap-6 font-bold">
         <p className="cursor-pointer text-gray-G30">{t("navbar.sign in")}</p>
-        <p className="rounded-full bg-primary-P300 w-auto  text-center text-white px-4 py-2 cursor-pointer">
+        <p className="rounded-full bg-primary-P300 w-auto  text-center text-white px-6 py-3 cursor-pointer">
           {t("navbar.sign up")}
         </p>
       </div>
