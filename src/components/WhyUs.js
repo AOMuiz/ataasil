2d3be1abd50b84c26ac7e5b902423b8cc8cd5899
AOMuiz <abdulwasiumuiz@gmail.com>
2022-11-09 18:29:09 +0100

    feat: convert why us list item into component and create an alternative styling choice for the list cards

--- src/components/WhyUs.js
+++ src/components/WhyUs.js
@@ -5,6 +5,7 @@ import dots1 from "/public/assets/images/Group 59.png";
 import dots2 from "/public/assets/images/Group 58.png";
 import useTranslation from "next-translate/useTranslation";
 import SectionHeader from "./SectionHeader";
+import SectionListCard from "./SectionListCard";
 
 const WhyUs = () => {
   const { t } = useTranslation("index");
@@ -14,41 +15,27 @@ const WhyUs = () => {
       <SectionHeader title={t("home.whyus.title")} />
       <div className="grid grid-cols-2 gap-8 justify-between">
         <section className="flex flex-col gap-3">
-          <div className="bg-white relative px-7 py-5">
-            <div className="bg-primary-P300 rounded-full absolute -right-1 top-2/4 -translate-y-2/4 translate-x-2/4 text-center h-12 w-12 flex items-center justify-center ">
-              <span className="text-2xl text-white">01</span>
-            </div>
-            <p className="text-lg pb-5 font-normal">محتوى تدريبي مميز </p>
-            <p className="text-gray-G30">
-              تنفرد التأصيل بمحتوى تدريبي مميز وغني بالمعارف والمهارات، مُعَدٌّ
-              على يد كوكبة من الخبراء المتخصصين في مجالات عديدة، وَفْق أسس
-              ومعايير تدريبية ومنهجية مُعتمَدة.
-            </p>
-          </div>
-          <div className="bg-white relative px-7 py-5">
-            <div className="bg-primary-P300 rounded-full absolute -right-1 top-2/4 -translate-y-2/4 translate-x-2/4 text-center h-12 w-12 flex items-center justify-center ">
-              <span className="text-2xl text-white">02</span>
-            </div>
-            <p className="text-lg pb-5 font-normal">
-              خبراء متخصصون بمجالات عديدة
-            </p>
-            <p className="text-gray-G30">
-              قامت التأصيل باستقطاب كوكبة من الخبراء البارزين في مجالات التنمية
-              الإدارية لإعداد وتقديم المحتوى التدريبي الإلكتروني.
-            </p>
-          </div>
-          <div className="bg-white relative px-7 py-5">
-            <div className="bg-primary-P300 rounded-full absolute -right-1 top-2/4 -translate-y-2/4 translate-x-2/4 text-center h-12 w-12 flex items-center justify-center ">
-              <span className="text-2xl text-white">03</span>
-            </div>
-            <p className="text-lg pb-5 font-normal">
-              انطلق بالسرعة التي تناسبك
-            </p>
-            <p className="text-gray-G30">
-              لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول استنكار النشوة
-              وتمجيد الألم نشأت بالفعل، وسأعرض لك التفاصيل لتكتشف
-            </p>
-          </div>
+          <SectionListCard
+            title={"محتوى تدريبي مميز"}
+            content={
+              "تنفرد التأصيل بمحتوى تدريبي مميز وغني بالمعارف والمهارات، مُعَدٌّ على يد كوكبة من الخبراء المتخصصين في مجالات عديدة، وَفْق أسس ومعايير تدريبية ومنهجية مُعتمَدة."
+            }
+            number={"01"}
+          />
+          <SectionListCard
+            title={"خبراء متخصصون بمجالات عديدة"}
+            content={
+              "قامت التأصيل باستقطاب كوكبة من الخبراء البارزين في مجالات التنمية الإدارية لإعداد وتقديم المحتوى التدريبي الإلكتروني."
+            }
+            number={"02"}
+          />
+          <SectionListCard
+            title={"انطلق بالسرعة التي تناسبك"}
+            content={
+              "لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول استنكار النشوة وتمجيد الألم نشأت بالفعل، وسأعرض لك التفاصيل لتكتشف."
+            }
+            number={"03"}
+          />
         </section>
         <section className="flex flex-col">
           <div className="flex gap-1 items-end">
