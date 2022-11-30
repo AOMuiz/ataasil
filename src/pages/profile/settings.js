9598b1cce5b23844464d8d2dee012b312a50f824
AOMuiz <abdulwasiumuiz@gmail.com>
2022-11-30 23:22:20 +0100

    rename some of navbar items and corresponding page route

--- src/pages/profile/settings.js
+++ src/pages/profile/settings.js
@@ -2,7 +2,7 @@ import React from "react";
 import ProfileLayout from "../../layout/profileLayout";
 
 const User = () => {
-  return <div>User</div>;
+  return <div>User Settings</div>;
 };
 
 User.PageLayout = ProfileLayout;
