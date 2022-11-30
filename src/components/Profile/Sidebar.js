6d2aeb3e10433a846dec20206383ba77eb3aa046
AOMuiz <abdulwasiumuiz@gmail.com>
2022-11-30 23:00:46 +0100

    remove border styling when link is not active

--- src/components/Profile/Sidebar.js
+++ src/components/Profile/Sidebar.js
@@ -16,6 +16,11 @@ const routes = [
     icon: "user",
   },
   {
+    slug: "/profile/book",
+    label: "نظرة عامة",
+    icon: "book",
+  },
+  {
     slug: "/profile/testimonials",
     label: "الشهادات",
     icon: "dollar",
@@ -34,11 +39,6 @@ const routes = [
     slug: "/profile/favorites",
     label: "المفضلة",
     icon: "favorite",
-  },
-  {
-    slug: "/profile",
-    label: "نظرة عامة",
-    icon: "home",
   },
   {
     slug: "/profile/bookmark",
@@ -76,9 +76,12 @@ const Sidebar = () => {
 export default Sidebar;
 
 const SidebarItem = styled.li(({ active }) => [
-  tw`flex gap-2 items-center text-gray-G30 py-2 px-2 border-l-4 cursor-pointer`,
+  tw`flex gap-2 items-center text-gray-G30 py-2 px-2  cursor-pointer`,
   active &&
     `background: linear-gradient(90deg, #A6D1F7 0%, rgba(166, 209, 247, 0) 100%);
-  border-color: #016FD0;
+     border-color: #016FD0;
+     font-weight:700;
+     color: #016FD0;
+     border-left: 4px solid;
   `,
 ]);
