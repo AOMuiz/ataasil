432aedf3494decb64ca01cde5daa8beab9e48fe6
Lukman10a <abdulrauflukman9@gmail.com>
2022-12-12 18:50:19 +0100

    changed the tw to className

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -10,43 +10,43 @@ const SignUp = () => {
 
   return (
     <>
-      <div tw="flex bg-neutral-N20">
-        <div tw="bg-white shadow-lg	 w-1/2 px-20 py-20">
-          <h1 tw="font-bold	text-3xl py-4">{t("sign up page.title")}</h1>
+      <div className="flex bg-neutral-N20">
+        <div className="bg-white shadow-lg	 w-1/2 px-20 py-20">
+          <h1 className="font-bold	text-3xl py-4">{t("sign up page.title")}</h1>
           <div>
-            <p tw="py-4">{t("sign up page.nationality")}</p>
+            <p className="py-4">{t("sign up page.nationality")}</p>
             <input
               type="text"
               placeholder={t("sign up page.nationality")}
-              tw="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+              className="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
             />
           </div>
 
-          <div tw="py-6">
-            <p tw="py-4">{t("sign up page.job")}</p>
+          <div className="py-6">
+            <p className="py-4">{t("sign up page.job")}</p>
             <input
               type="text"
               placeholder={t("sign up page.job")}
-              tw="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+              className="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
             />
           </div>
 
-          <div tw="py-2">
-            <p tw="py-4">{t("sign up page.number")}</p>
+          <div className="py-2">
+            <p className="py-4">{t("sign up page.number")}</p>
             <input
               type="text"
               placeholder={t("sign up page.number")}
-              tw="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+              className="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
             />
           </div>
 
-          <p tw="py-4">{t("sign up page.date")}</p>
-          <div tw="py-2 flex gap-6">
+          <p className="py-4">{t("sign up page.date")}</p>
+          <div className="py-2 flex gap-6">
             <div>
               <input
                 type="text"
                 placeholder={t("sign up page.day")}
-                tw="border	w-[167px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+                className="border	w-[167px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
               />
             </div>
 
@@ -54,7 +54,7 @@ const SignUp = () => {
               <input
                 type="text"
                 placeholder={t("sign up page.month")}
-                tw="border	w-[167px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+                className="border	w-[167px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
               />
             </div>
 
@@ -62,105 +62,105 @@ const SignUp = () => {
               <input
                 type="text"
                 placeholder={t("sign up page.year")}
-                tw="border	w-[167px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+                className="border	w-[167px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
               />
             </div>
           </div>
 
-          <div tw="flex gap-10">
-            <div tw="py-2">
-              <p tw="py-4">{t("sign up page.phone number")}</p>
+          <div className="flex gap-10">
+            <div className="py-2">
+              <p className="py-4">{t("sign up page.phone number")}</p>
               <input
                 type="text"
                 placeholder={t("sign up page.phone number")}
-                tw="border	w-[344px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+                className="border	w-[344px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
               />
             </div>
 
-            <div tw="py-2">
-              <p tw="py-4">{t("sign up page.code")}</p>
+            <div className="py-2">
+              <p className="py-4">{t("sign up page.code")}</p>
               <input
                 type="text"
                 placeholder={t("sign up page.number")}
-                tw="border	w-[167px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+                className="border	w-[167px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
               />
             </div>
           </div>
 
-          <div tw="py-6">
-            <p tw="py-4">{t("sign up page.email")}</p>
+          <div className="py-6">
+            <p className="py-4">{t("sign up page.email")}</p>
             <input
               type="text"
               placeholder={t("sign up page.email")}
-              tw="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+              className="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
             />
           </div>
 
-          <div tw="py-6">
-            <p tw="py-4">{t("sign up page.email confirmation")}</p>
+          <div className="py-6">
+            <p className="py-4">{t("sign up page.email confirmation")}</p>
             <input
               type="text"
               placeholder={t("sign up page.email confirmation")}
-              tw="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+              className="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
             />
           </div>
 
-          <div tw="py-6">
-            <p tw="py-4">{t("sign up page.password")}</p>
+          <div className="py-6">
+            <p className="py-4">{t("sign up page.password")}</p>
             <input
               type="text"
               placeholder={t("sign up page.password")}
-              tw="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+              className="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
             />
           </div>
 
-          <div tw="py-6">
-            <p tw="py-4">{t("sign up page.password confirmation")}</p>
+          <div className="py-6">
+            <p className="py-4">{t("sign up page.password confirmation")}</p>
             <input
               type="text"
               placeholder={t("sign up page.password confirmation")}
-              tw="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
+              className="border	w-[552px] h-[60px] p-4 rounded-md bg-primary-P700 border-black"
             />
           </div>
 
-          <div tw="flex justify-around items-center w-[329px] h-32 border rounded-md my-10 bg-primary-P700 border-black">
+          <div className="flex justify-around items-center w-[329px] h-32 border rounded-md my-10 bg-primary-P700 border-black">
             <div>
               <Image src={robot} alt="" />
             </div>
-            <div tw="flex gap-2 rounded-md">
+            <div className="flex gap-2 rounded-md">
               <p>I am not a robot</p>
-              <div tw="border-black border h-8 w-8 cursor-pointer bg-primary-P700"></div>
+              <div className="border-black border h-8 w-8 cursor-pointer bg-primary-P700"></div>
             </div>
           </div>
 
           <div>
-            <div tw="flex gap-2 py-2">
-              <div tw="border-black border h-6 w-6"></div>
+            <div className="flex gap-2 py-2">
+              <div className="border-black border h-6 w-6"></div>
               <div className="text-lg	">
                 {t("sign up page.email notification")}
               </div>
             </div>
-            <div tw="flex gap-2">
-              <div tw="border-black border h-6 w-6"></div>
+            <div className="flex gap-2">
+              <div className="border-black border h-6 w-6"></div>
               <div className="text-lg	">
                 {t("sign up page.text notification")}
               </div>
             </div>
-            <div tw="flex gap-2 py-2">
-              <div tw="border-black border h-6 w-6"></div>
+            <div className="flex gap-2 py-2">
+              <div className="border-black border h-6 w-6"></div>
               <div className="text-lg	">{t("sign up page.agreement")}</div>
             </div>
           </div>
 
           <div className="flex justify-between my-4">
-            <div tw="text-center text-white py-2 rounded-3xl bg-[#D5D5D5] w-40 h-10">
+            <div className="text-center text-white py-2 rounded-3xl bg-[#D5D5D5] w-40 h-10">
               {t("sign up page.create account")}
             </div>
             <div>{t("sign up page.account")}</div>
           </div>
         </div>
 
-        <div tw="">
+        <div>
           <Image src={signupframe} alt="" />
         </div>
       </div>
